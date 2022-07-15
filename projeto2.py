e85f49b8cd0d87bab92327be2f44c2b6411351e1
Pedro <105446915+UIy2KHDkF6WPXx3k83Df8w@users.noreply.github.com>
2022-07-15 02:04:26 -0300

    Add files via upload

---
+++ projeto2.py
@@ -0,0 +1,119 @@
+print("paineArtText")
+
+print(f'''
+isso daqui é como se fosse um google só que ART TEXT
+dando uma explicação rápida: desenho em texto
+''')
+print(f'''
+  ________    _____
+ /\/\/\/\/\  | "º  \\
+<|\/\/\/\/\|_/ /___/
+ |____________/
+ |_|_|   /_/_/
+''')
+
+print(f'''
+[99] disponível
+[0] sair
+
+os animais eu peguei no github do meu colega: sacadsii 
+''')
+
+loop="positivo"
+while loop == "positivo":
+
+    resposta=input("pesquisa o animal: ")
+
+    if resposta == "raposa":
+        print(f'''
+
+ ^...^ 
+<_* *_> 
+  \_/ 
+        ''')
+
+
+
+    if resposta == "cavalo":
+        print(f'''
+,~~_
+|/\ =_ _ ~
+ _( )_( )\~~
+ \,\  _|\ \~~~
+    \`   \
+    `    `
+
+
+            .''
+  ._.-.___.' (`\
+ //(        ( `'
+'/ )\ ).__. ) 
+' <' `\ ._/'\
+   `   \     \
+        ''')
+
+    if resposta == "aranha magrela":
+        print(f'''
+                   /\ 
+                  /  \ 
+                 |  _ \                  _ 
+                 | / \ \                / \ 
+                 |/   \ \              /   \ 
+                 /     \ |        /\  /     \ 
+                /|      \| ~  ~  /  \/       \ 
+        _______/_|_______\(o)(o)/___/\_____   \ 
+       /      /  |       (______)     \    \   \_ 
+      /      /   |                     \    \ 
+     /      /    |                      \    \ 
+    /      /     |                       \    \ 
+   /     _/      |                        \    \ 
+  /             _|                         \    \_ 
+_/                                          \ 
+                                             \ 
+                                              \_ 
+        ''')
+
+    if resposta == "aranhas coletivo":
+        print(f'''
+                                   _ 
+       /      \         __      _\( )/_ 
+    \  \  ,,  /  /   | /  \ |    /(O)\  
+     '-.`\()/`.-'   \_\\  //_/    _.._   _\(o)/_  //  \\ 
+    .--_'(  )'_--.   .'/()\'.   .'    '.  /(_)\  _\\()//_ 
+/ /` /`""`\ `\ \ \\ // / __ \ / // \\ \ 
+|  |  >< |  |  , |  >< |  , |  \__/ | 
+    \  \      /  /         . \  \      /  / .              _ 
+   _    '.__.'    _\(O)/_   \_'--`(  )'--'_/     __     _\(_)/_ 
+_\( )/_            /(_)\      .--'/()\'--.    | /  \ |   /(O)\ 
+ /(O)\  //  \\         _     /  /` '' `\  \  \_\\  //_/ 
+       _\\()//_     _\(_)/_    |        |      //()\\  
+ jgs  / //  \\ \     /(o)\      \      /       \\  // 
+       | \__/ |         
+        ''')
+
+
+
+
+
+
+    elif resposta == "99":
+        print(f'''
+    raposa [tem]
+    vaca [ainda não]
+    panda [ainda não]
+    cavalo [tem]
+    elefante [ainda não]
+    aranha magrela [tem]
+    aranhas coletivo [tem]
+
+
+        ''')
+
+
+
+    elif resposta == "0":
+        print ("saindo ...")
+        
+        loop="negativo"
+
+        #tá em beta
